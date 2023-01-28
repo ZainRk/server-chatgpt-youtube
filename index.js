8033cbd31155e9c63546b1e46fe18cdbface43a3
ZainKeepsCode <ahmadrao925@gmail.com>
2023-01-28 18:48:29 +0500

    initial

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import env from 'dotenv'
+import {Configuration, OpenAIApi} from 'openai'
+
+const app = express()
+
+env.config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+// Configure open api
+const configuration = new Configuration({
+    organization: "org-cdScfWEjgQar9o8rSCAC2t9S",
+    apiKey: process.env.API_KEY
+})
+const openai = new OpenAIApi(configuration)
+
+
+// listeninng
+app.listen("3080", ()=>console.log("listening on port 3080"))
+
+
+// dummy route to test
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+
+//post route for making requests
+app.post('/', async (req, res)=>{
+    const {message} = req.body
+
+    try{
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${message}`,
+            max_tokens: 100,
+            temperature: .5
+        })
+        res.json({message: response.data.choices[0].text})
+
+    }catch(e){
+        console.log(e)
+        res.send(e).status(400)
+    }
+})
