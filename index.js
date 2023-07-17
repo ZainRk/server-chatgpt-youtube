88fba14ef1a38e232f1709a27dbf01a25ed24882
arslantoor <muhammadarslantoor@gmail.com>
2023-07-17 07:57:38 +0500

    Update port

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const openai = new OpenAIApi(configuration)
 
 
 // listeninng
-app.listen("3080", () => console.log("listening on port 3080"))
+app.listen("5001", () => console.log("listening on port 3080"))
 
 
 // dummy route to test
